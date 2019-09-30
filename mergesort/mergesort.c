94f282ce71314e28aec3583a4030674b4eed6215
monjo003 <monjo003@morris.umn.edu>
2019-09-30 12:38:30 -0500

    adding the mergeRange function to sort, the smaller arrays and conbines them to create a final array

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -7,3 +7,43 @@ bool needsSorting(int rangeSize)
 {
 	return rangeSize >= 2;
 }
+void mergeRanges(int values[], int startIndex, int midPoint, int endIndex)
+{
+	const int rangeSize = endIndex - startIndex;
+	int *destination = (int*) calloc(rangeSize + 1, sizeof(int));
+	int firstIndex = startIndex;
+	int secondIndex = midPoint;
+	int copyIndex = 0;
+	while(firstIndex < midPoint && secondIndex < endIndex)
+	{
+		if(values[firstIndex] < values[secondIndex])
+		{
+			destination[copyIndex] = values[firstIndex];
+			firstIndex++;
+		}
+		else
+		{
+			destination[copyIndex] = values[secondIndex];
+			secondIndex++;
+		}
+		copyIndex++;
+	}
+	while(firstIndex < midPoint)
+	{
+		destination[copyIndex] = values[firstIndex];
+		copyIndex++;
+		firstIndex++;
+	}
+	while (secondIndex < endIndex)
+	{
+		destination[copyIndex] = values[secondIndex];
+		++copyIndex;
+		++secondIndex;
+	}
+	destination [rangeSize] = '\0';
+	for(int i = 0; i < rangeSize; i++)
+	{
+		values [i + startIndex] = destination[i];
+	}
+	free(destination);
+}
